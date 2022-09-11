3b1270261b21f0950756fbf7d5404ffe86c974ab
LaszloSomogyi <slsomogyilaszlo@gmail.com>
2022-09-11 18:04:26 +0200

    Repo corrections

--- src/main/java/com/example/canteen/repo/PurchaseItemRepo.java
+++ src/main/java/com/example/canteen/repo/PurchaseItemRepo.java
@@ -3,5 +3,9 @@ package com.example.canteen.repo;
 import com.example.canteen.model.PurchaseItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PurchaseItemRepo extends JpaRepository<PurchaseItem, Long> {
+
+    Optional<PurchaseItem> findPurchaseItemById(Long id);
 }
